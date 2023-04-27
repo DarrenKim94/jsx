07949be83271b48b7c8217c40ff26296392bce95
Darren Kim <dkim555@yahoo.com>
2023-04-26 18:53:48 -0700

    import and export components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+//Import the React and ReactDom libraries
 import React from "react";
 import ReactDOM from 'react-dom/client';
+import App from "./App";
 
+//Get a reference to the div with ID root
 const el = document.getElementById('root');
-const root = React.createRoot(el);
 
-function App() {
-    return <h1>Hi There</h1>
-}
+//Tell React to take control of the element
+const root = ReactDOM.createRoot(el);
 
+//Show the component on the screen
 root.render(<App />); 
